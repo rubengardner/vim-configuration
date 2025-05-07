40aee1cdedc90d60573389862b04fdf48f382eef
rubengardner <rgardnercuesta@gmail.com>
2025-05-07 17:49:42 +0200

    Add global python for tests

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,17 +1,24 @@
 return {
   "vim-test/vim-test",
   config = function()
-    -- Path to the Poetry-managed Python interpreter
-    local python = vim.fn.expand("~/Library/Caches/pypoetry/virtualenvs/badger-go-5Azyc4AC-py3.12/bin/python")
-
-    -- Set environment variables for Django TestCase
-    vim.fn.setenv("PYTHONUNBUFFERED", "1")
+    local python_path = vim.fn.trim(vim.fn.system("poetry run which python"))
     vim.fn.setenv("DJANGO_SETTINGS_MODULE", "badgermapping.settings_local")
+    local cwd = vim.fn.getcwd()
 
-    -- Set the Python interpreter to the Poetry virtualenv for both unit and Django tests
-    vim.g["test#python#python_path"] = python
+    local project_root = cwd
+    while
+      not vim.fn.filereadable(project_root .. "/pyproject.toml")
+      and not vim.fn.filereadable(project_root .. "/setup.py")
+    do
+      project_root = vim.fn.fnamemodify(project_root, ":h")
+      if project_root == "/" then
+        break
+      end
+    end
 
-    -- Set test runner to pytest for unit tests (if using pytest)
+    vim.fn.setenv("PYTHONPATH", project_root)
+    vim.fn.setenv("PYTHONUNBUFFERED", "1")
+    vim.g["test#python#python_path"] = python_path
     vim.g["test#python#runner"] = "pytest"
   end,
 }
