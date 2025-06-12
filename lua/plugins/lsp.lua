0ea1dc0aa7330654aa7ab58468c4d491f17b3cd8
rubengardner <rgardnercuesta@gmail.com>
2025-06-12 08:37:18 +0200

    LSP configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,14 @@ return {
       severity_sort = true,
     })
 
-    local python_path = vim.fn.trim(vim.fn.system("poetry run which python"))
+    local poetry_python = vim.fn.trim(vim.fn.system("poetry env info -p 2>/dev/null"))
+    local python_path = ""
+
+    if poetry_python ~= "" then
+      python_path = poetry_python .. "/bin/python"
+    else
+      python_path = vim.fn.trim(vim.fn.system("which python3"))
+    end
 
     require("lspconfig").pyright.setup({
       settings = {
@@ -20,5 +27,17 @@ return {
     })
 
     require("lspconfig").tsserver.setup({})
+
+    require("lspconfig").gopls.setup({
+      settings = {
+        gopls = {
+          analyses = {
+            unusedparams = true,
+          },
+          staticcheck = true,
+          gofumpt = true,
+        },
+      },
+    })
   end,
 }
