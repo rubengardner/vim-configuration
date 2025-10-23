ee20827eadb3ae56c296a525a79c3c7244c46e96
rubengardner <rgardnercuesta@gmail.com>
2025-10-23 13:48:16 +0200

    Refactor find test to find file not class

--- lua/plugins/python-find-test.lua
+++ lua/plugins/python-find-test.lua
@@ -4,26 +4,17 @@ return {
     local telescope = require("telescope.builtin")
 
     vim.api.nvim_create_user_command("FindTest", function()
-      -- Search for class definition in the current file
-      local class_name = vim.fn.search("class \\(\\w\\+\\)", "bn") -- "bn" searches backward, non-wrapping
-      if class_name == 0 then
-        print("No class found!")
+      local file_path = vim.api.nvim_buf_get_name(0)
+      if file_path == "" then
+        print("No file open!")
         return
       end
 
-      -- Extract class name
-      local line = vim.fn.getline(class_name)
-      local match = line:match("class%s+(%w+)")
-      if not match then
-        print("Couldn't extract class name!")
-        return
-      end
+      local file_name = vim.fn.fnamemodify(file_path, ":t") -- extract filename
+      local test_file = "test_" .. file_name
 
-      local test_class = match .. "TestCase"
-
-      -- Use Telescope's live_grep instead of grep
-      telescope.live_grep({
-        default_text = test_class, -- Prefill search with class name
+      telescope.find_files({
+        default_text = test_file, -- prefill with test filename
       })
     end, {})
   end,
